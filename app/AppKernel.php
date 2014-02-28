30e29f37d9f76b16c37126ac048297dc2fabad41
Adam Misiorny <adam.misiorny@gmail.com>
2014-02-28 21:47:40 +0100

    add bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
+            new Acme\Bundle\MysqlBundle\AcmeMysqlBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Acme/Bundle/MysqlBundle/AcmeMysqlBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Acme\Bundle\MysqlBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AcmeMysqlBundle extends Bundle
+{
+}
